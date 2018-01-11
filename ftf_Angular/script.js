54ecdf284051a11c92593f1ddc9732248039d4fa
njlie <njlie@ucsc.edu>
2018-01-11 01:48:39 -0800

    Added portrait images and repeated flip cards for two bots.

--- ftf_Angular/script.js
+++ ftf_Angular/script.js
@@ -18,13 +18,57 @@
 
     		.when('/contact', {
     			templateUrl : 'pages/contact.html',
-    			controler   : 'contactController'
+    			controller   : 'contactController'
     		});
     });
 
-    // create the controller and inject Angular's $scope
-    ftfApp.controller('mainController', function($scope) {
+    ftfApp.factory('mainFactory', function ($http, $q) {
+        var service = {};
+        service.getBots = function () {
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'bots.json',
+                cache: false
+            }).success(function (data) {
+                deferred.resolve(data);
+            });
 
+            return deferred.promise;
+        }
+
+        return service;
+    })
+
+    // create the controller and inject Angular's $scope
+    ftfApp.controller('mainController', function($scope, $http, mainFactory) {
+
+        $http.defaults.cache = false;
+
+        $scope.hover = function(state) {
+            if(state) {
+                element.addClass('change');
+            }
+        }
+
+        // $http({
+        //     method: 'GET',
+        //     url: 'bots.json?random='+ new Date().getTime(),
+        //     cache: false
+        // }).then(function(res) {
+        //     $scope.bots = res.data;
+        //     console.log($scope.bots);
+        // });
+
+        // mainFactory.getBots().then(function (data) {
+        //     $scope.bots = data;
+        // });
+
+        $http.get('bots.json?random=' + new Date().getTime()).success(function (response) {
+            $scope.bots = response;
+        })
+
+        console.log($scope.bots);
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
     });
